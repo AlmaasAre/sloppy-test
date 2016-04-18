ff48f9fb146af6d86753522ebff528b1ee40f967
Are Almaas <arealmaas@gmail.com>
2016-04-18 11:36:06 +0200

    fix users

--- user-management/app.js
+++ user-management/app.js
@@ -8,9 +8,9 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const routes = require('./routes/index');
-const users = require('./routes/users');
 
 const app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +25,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
+require('./routes/api')(app);
 
 // catch 404 and forward to error handler
 app.use((req, res, next)=> {
